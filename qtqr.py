5da84b4d538b5e4802c203f77184b0d4d36864e7
Ramiro Algozino <algozino@gmail.com>
2014-01-21 22:49:48 -0300

    Updated version information for 1.4 release.

--- qtqr.py
+++ qtqr.py
@@ -26,7 +26,7 @@ __email__ = "algozino@gmail.com"
 __copyright__ = "copyright (C) 2011 Ramiro Algozino"
 __credits__ = "David Green"
 __license__ = "GPLv3"
-__version__ = "1.3"
+__version__ = "1.4"
 
 class MainWindow(QtGui.QMainWindow):
     def __init__(self):
